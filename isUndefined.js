a3c2b2620e8da8c3101d3e1564e8a774170019c0
Lorenzo Turrino <neuronefunzionante@gmail.com>
2019-01-17 19:23:11 +0000

    add quick instructions

--- isUndefined.js
+++ isUndefined.js
@@ -19,3 +19,15 @@ function isUndefinedHelper (value) {
 
 // make the function available to be used somewhere else
 module.exports = isUndefined;
+
+
+/*
+  This function is quite messy, why don't we refactor it!? (=== improve it to be better, more understandable)
+  before you do that, you will want to actually ensure that you're not breaking functionality. Tests!
+
+  * you'll need to install jest. there is no package.json at the moment
+  * start creating the files and writing assertion that capture the functionaly of the code (up to you to find it)
+  * once done, you can start changing the function in successive steps, running test every change you make to ensure
+  that everything is correct
+
+*/
